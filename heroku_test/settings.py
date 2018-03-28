4590fc36158bf98b49a29354ee89d1f892c24f6c
TAKAHASHI Shuuji <shuuji3@gmail.com>
2018-03-28 21:41:26 +0900

    Update settings for heroku deplay.

--- heroku_test/settings.py
+++ heroku_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&+#_$+%@kze!k40o%3_j4np!(si8cmiuej+b-^5azyruce#uad'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shuuji3-heroku-django-test.herokuapp.com']
 
 
 # Application definition
@@ -117,4 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
